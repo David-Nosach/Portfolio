2a8e6335130af42ff1617a8b5f82db659960e4f8
David-Nosach <91391933+David-Nosach@users.noreply.github.com>
2022-09-27 21:56:18 -0700

    Final Commit hopefully

---
+++  app.js
@@ -0,0 +1,27 @@
+arrowTop = document.getElementById("upArrow");
+document.getElementsByClassName("show");
+document.getElementsByClassName("hide");
+
+arrowTop.className = "hide";
+
+// arrow appears as you scroll function
+
+let scrolling = function () {
+
+    let y = window.scrollY;
+    if (y > 70) {
+        arrowTop.className = "show"
+    } else {
+        arrowTop.className = "hide"
+    }
+};
+
+window.addEventListener("scroll", scrolling);
+
+// Back to top function
+
+arrowTop.addEventListener("click", backToTop);
+
+function backToTop() {
+    window.scrollTo(0, 0);
+}
